7f7a2d962701f307036672f7c98d636026b266f6
Administrator <alex.radzyuk@gmail.com>
2017-03-27 20:09:48 +0300

    List with overdue was built

--- main.rb
+++ main.rb
@@ -26,18 +26,22 @@ issues = []; client.Issue.all.each { |i| issues << i if i.project.name.include?(
 date_today = Time.now.to_s[0..9].gsub(/\-/, '/')
 
 # Delete if difference between start and current date less than 5 days:
-issues.delete_if { |i| (Date.parse(date_today).mjd - Date.parse(i.customfield_10100[0..9].gsub(/\-/, '/')).mjd) < 5}
+issues.delete_if { |i| (Date.parse(date_today).mjd - Date.parse(i.customfield_10100[0..9].gsub(/\-/, '/')).mjd) < 5 }
+
+# Keep if state is 'Open' or 'To do':
+issues.keep_if { |i| i.status.name.match('Open') || i.status.name.match('To do') }
 
 puts "---------------------------------------"
 
-puts " Overdue issues count: #{issues.count}"
+puts "\n Overdue issues count: #{issues.count} (More than 5 days without any action)"
 
 issues.each do |i|
 	puts "\n Issue: #{i.summary} (Project: #{i.project.name})"
-	puts " Date today: #{date_today}"
-	puts " Planning start date: #{i.customfield_10100[0..9]} "
-	puts " Updated: #{i.updated[0..9]}"
+	puts " issue status: #{i.status.name}"
+	puts " Date today:          #{date_today}"
+	puts " Planning start date: #{i.customfield_10100[0..9].gsub(/\-/, '/')} "
+	puts " Link: #{i.remotelink.all}"
 end
 
-puts "---------------------------------------"
+puts "\n---------------------------------------"
 
