d56cbd3cff8f2d8b90f98dc614f98e920c962f54
Administrator <alex.radzyuk@gmail.com>
2017-03-27 16:37:40 +0300

    building a list of overdue issues

--- main.rb
+++ main.rb
@@ -1,16 +1,13 @@
 #!/usr/bin/env ruby 
 
-require 'rubygems'
 require 'pp'
 require 'jira-ruby'
 require 'dotenv'
 
 # Create .env in the root and specify your vars there, then load it here:
-
 Dotenv.load('.env')
 
 # Set some configurations for JIRA access:
-
 options = {
             :username => ENV['JIRA_USER'],
             :password => ENV['JIRA_PASS'],
@@ -21,22 +18,26 @@ options = {
           }
 
 # Make connect:
-
 client = JIRA::Client.new(options)
 
-# CONSTANTA with regular expression:  
-
-PATTERN = /[PATTERN]/
-
 # Build array with issues from all projects except those that are PATTERN's:
+issues = []; client.Issue.all.each { |i| issues << i if i.project.name.include?('Pattern') != true && i.customfield_10100}
 
-issues = []; issues << client.Issue.all.each {|i| issues << i if i.key != PATTERN}
+date_today = Time.now.to_s[0..9].gsub(/\-/, '/')
 
+# Delete if difference between start and current date less than 5 days:
+issues.delete_if { |i| (Date.parse(date_today).mjd - Date.parse(i.customfield_10100[0..9].gsub(/\-/, '/')).mjd) < 5}
 
-#puts " Deadline: #{issues[1].duedate}"
-puts " Updated: #{issues[1].updated[0..9]}"
-puts " Planning start date: #{issues[1].customfield_10100[0..9]}"
+puts "---------------------------------------"
 
-difference = Date.parse(issues[1].updated[0..9].gsub(/\-/, '/')).mjd - Date.parse(issues[1].customfield_10100[0..9].gsub(/\-/, '/')).mjd
+puts " Overdue issues count: #{issues.count}"
 
-puts " The difference is: #{difference}"
+issues.each do |i|
+	puts "\n Issue: #{i.summary} (Project: #{i.project.name})"
+	puts " Date today: #{date_today}"
+	puts " Planning start date: #{i.customfield_10100[0..9]} "
+	puts " Updated: #{i.updated[0..9]}"
+end
+
+puts "---------------------------------------"
+
