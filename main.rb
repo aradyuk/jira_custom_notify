b9733fc4a3408aac8cd7b5290f3f9384dbbe88c8
Administrator <alex.radzyuk@gmail.com>
2017-03-28 09:47:33 +0300

    Small fix

--- main.rb
+++ main.rb
@@ -37,10 +37,10 @@ client = JIRA::Client.new(jira_options)
 # Build array with issues from all projects except those that are PATTERN's:
 issues = []; client.Issue.all.each { |i| issues << i if i.project.name.include?('Pattern') != true && i.customfield_10100}
 
-date_today = Time.now.to_s[0..9].gsub(/\-/, '/')
+date_today = Time.now.to_s[0..9]
 
 # Delete if difference between start and current date less than 5 days:
-issues.delete_if { |i| (Date.parse(date_today).mjd - Date.parse(i.customfield_10100[0..9].gsub(/\-/, '/')).mjd) < 5 }
+issues.delete_if { |i| (Date.parse(date_today.gsub(/\-/, '/')).mjd - Date.parse(i.customfield_10100[0..9].gsub(/\-/, '/')).mjd) < 5 }
 
 # Keep if state is 'Open' or 'To do':
 issues.keep_if { |i| i.status.name.match('Open') || i.status.name.match('To do') }
@@ -53,7 +53,7 @@ issues.each do |i|
 	puts "\n Issue: #{i.summary} (Project: #{i.project.name})"
 	puts " issue status: #{i.status.name}"
 	puts " Date today:          #{date_today}"
-	puts " Planning start date: #{i.customfield_10100[0..9].gsub(/\-/, '/')} "
+	puts " Planning start date: #{i.customfield_10100[0..9]}"
 	puts " Link: #{i.remotelink.all}"
 
 
@@ -66,10 +66,10 @@ puts "\n---------------------------------------"
 #     from ENV['MAIL_ADDRESS']
 #  subject 'Overdue issues in Jira-Marketing'
 #     body "\n 
-# 					Overdue issues count: #{issues.count} (More than 5 days without any action)\n
-#					Issue: #{i.summary} (Project: #{i.project.name}\n
-#					issue status: #{i.status.name}\n
-#					Date today:          #{date_today}\n
-#					Planning start date: #{i.customfield_10100[0..9].gsub(/\-/, '/')}\n
-#					Link: #{i.remotelink.all}"; end
-End
+# 		 	Overdue issues count: #{issues.count} (More than 5 days without any action)\n
+#			Issue: #{i.summary} (Project: #{i.project.name}\n
+#			issue status: #{i.status.name}\n
+#			Date today:          #{date_today}\n
+#			Planning start date: #{i.customfield_10100[0..9].gsub(/\-/, '/')}\n
+#			Link: #{i.remotelink.all}"; end
+end
